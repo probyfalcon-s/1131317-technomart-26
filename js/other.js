260aeb37fca9936ac23fef303b08ed3adc8d41f8
Alex Sokolov <cokol2122@yandex.ru>
2019-10-16 02:39:16 +0300

    Блок сервисы
    1. Доставка ссылка и блок теперь в состоянии current
    2. Корректно отображается active для этих ссылок "гарантия","Кредит"

--- js/other.js
+++ js/other.js
@@ -7,6 +7,9 @@ var guaranteeSub = document.querySelector(".sub-menu-2");
 var credit = document.querySelector(".button-credit");
 var creditSub = document.querySelector(".sub-menu-3");
 
+var current = document.querySelector(".current-delivery");
+var currentLink = document.querySelector(".current-delivery-link");
+
 
   delivery.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -19,6 +22,8 @@ var creditSub = document.querySelector(".sub-menu-3");
   evt.preventDefault();
   deliverySub.classList.remove("service-show");
   creditSub.classList.remove("service-show");
+  current.classList.remove("current-delivery");
+  currentLink.classList.remove("current-delivery-link");
   guaranteeSub.classList.add("service-show");
  });
 
@@ -26,6 +31,8 @@ var creditSub = document.querySelector(".sub-menu-3");
   evt.preventDefault();
   deliverySub.classList.remove("service-show");
   guaranteeSub.classList.remove("service-show");
+  current.classList.remove("current-delivery");
+  currentLink.classList.remove("current-delivery-link");
   creditSub.classList.add("service-show");
 });
 
